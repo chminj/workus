f6b453a6e5c68e3877e3667bf4fbaee1f3c75c22
vivatube <dltkdgus2008@naver.com>
2024-11-20 11:54:22 +0900

    feat : 로그아웃 버튼 및 회원명
    로그아웃 버튼 및 회원명 우측상단에 추가
    
    Modal 대신 부트스트랩 아이콘으로 대체

--- src/main/java/com/example/workus/controller/HomeController.java
+++ src/main/java/com/example/workus/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.example.workus.controller;
 
+import com.example.workus.security.LoginUser;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
